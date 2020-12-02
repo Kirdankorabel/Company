83aa31792cb57883a3c06e32f04c8181fe863956
Kirdankorabel <74896893+Kirdankorabel@users.noreply.github.com>
2020-12-02 21:43:36 +0300

    Добавьте файлы проекта.

---
+++ Company/Model/Position.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Model
+{
+    public class Position
+    {
+        public string Name { get; }
+        public int Salary { get; }
+
+        public Position(string name, int salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        static string director = "director";
+        static string leader = "leader";
+        static string engineer = "engineer";
+        static string handyman = "handyman";
+
+        public static Position Director = new Position(director, 50000);
+        public static Position Leader = new Position(leader, 20000);
+        public static Position Engineer = new Position(engineer, 10000);
+        public static Position Handyman = new Position(handyman, 5000);
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
